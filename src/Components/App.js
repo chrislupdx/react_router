e97eefe103dde45f5c48b9206a22ec447962f834
dreamer <wildetangeant@gmail.com>
2019-05-20 11:35:25 -0700

    header rendering

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route
-// } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import Header from './Header';
 
 export default function App() {
-  <Header />;
+  return (
+    <Router>
+        <>
+        <Header />
+        </>
+    </Router>
+  );
 }
+
+
